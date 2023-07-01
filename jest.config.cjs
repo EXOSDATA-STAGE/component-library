12abda2edface1d1b27060434f59026a03971a1e
Elab4d1 <hassanflv@gmail.com>
2023-07-01 18:18:59 +0100

    convert configs extension to commonjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,7 @@
-const path = require("path");
-
 module.exports = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     ".(css|scss)$": "identity-obj-proxy",
-    "^@/lib/utils$": path.resolve(__dirname, "src/lib/utils"),
+    "^@/lib/utils$": "<rootDir>/src/lib/utils",
   },
 };
