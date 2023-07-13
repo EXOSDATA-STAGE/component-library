e19324efcbd2cc01302f337a6e438afb8c389cdf
Elab4d1 <hassanflv@gmail.com>
2023-07-13 12:48:06 +0100

    add button styles

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
-  title: "Example/Button",
+  title: "Button",
   component: Button,
 } satisfies Meta<typeof Button>;
 
@@ -12,5 +12,14 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     children: "Click Here",
+    variant: "primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Click Here",
+    variant: "secondary",
+    disabled: true,
   },
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,49 @@ import React, { forwardRef } from "react";
 import "./Button.css";
 import { cn } from "@/lib/utils";
 
+import { VariantProps, cva } from "class-variance-authority";
+
+const buttonVariants = cva("btn", {
+  variants: {
+    variant: {
+      primary: ["btn-primary"],
+      secondary: ["btn-secondary"],
+    },
+    size: {
+      xs: ["btn-xs"],
+      sm: ["btn-sm"],
+      md: ["btn-md"],
+      lg: ["btn-lg"],
+      xl: ["btn-xl"],
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+  },
+});
+
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   children?: React.ReactNode;
-  label?: string;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ label, className, children, ...props }, ref) => {
+  ({ className, variant, size, children, ...props }, ref) => {
     return (
-      <button ref={ref} className={cn("bigger-size", className)} {...props}>
+      <button
+        ref={ref}
+        className={cn(
+          buttonVariants({
+            variant: variant,
+            size: size,
+          }),
+          className
+        )}
+        {...props}
+      >
         {children}
-        {label}
       </button>
     );
   }
