dc573c9a8328d93e9aa18d56d9c7b12a6ad969e6
Elab4d1 <hassanflv@gmail.com>
2023-08-08 21:37:05 +0100

    Dropdown: add new component

---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuGroup,
+} from "./Dropdown";
+import { Button } from "@/components";
+
+const meta = {
+  title: "Dropdown Menu",
+  component: DropdownMenu,
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <DropdownMenuTrigger asChild>
+          <Button>Open Menu</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </>
+    ),
+  },
+};
--- src/components/Dropdown/index.ts
+++ src/components/Dropdown/index.ts
@@ -1,6 +1,3 @@
-import "@/styles/globals.css";
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
 export {
   DropdownMenu,
   DropdownMenuContent,
